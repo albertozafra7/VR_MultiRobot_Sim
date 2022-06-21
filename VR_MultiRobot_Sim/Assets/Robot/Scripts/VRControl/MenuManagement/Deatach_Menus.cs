e099342df3752d24dc6095d50a789a3946952fe8
albertozafra7 <albertozafra7@gmail.com>
2022-06-21 20:55:14 +0200

    Robot Joint Menu Deatachment from the hand by grasping

---
+++ VR_MultiRobot_Sim/Assets/Robot/Scripts/VRControl/MenuManagement/Deatach_Menus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deatach_Menus : MonoBehaviour
+{
+    public OVRGrabbable This_Menu;
+    public RectTransform This_Transform;
+    public Transform robot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(This_Menu.isGrabbed){
+             transform.parent = null;
+             This_Transform.localPosition = This_Menu.grabbedBy.transform.position;
+             This_Transform.localRotation = This_Menu.grabbedBy.transform.rotation;
+        } else
+            transform.parent = robot;
+
+
+    }
+}
